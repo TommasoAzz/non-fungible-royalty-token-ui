0e27824147db460ae9a37d873ece8cc47bd4ee46
Tommaso Azzalin <azzalintommaso@gmail.com>
2022-01-19 09:54:10 +0100

    Fixed imports

--- lib/views/collections/collections_view.dart
+++ lib/views/collections/collections_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:non_fungible_royalty_token_marketplace_ui/views/collections/collection_content_tablet.dart';
+import 'collection_content_tablet.dart';
 import 'collections_content_mobile.dart';
 import 'collections_content_desktop.dart';
 import 'package:responsive_builder/responsive_builder.dart';
--- lib/views/create/create_view.dart
+++ lib/views/create/create_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../constants/app_colors.dart';
 import '../../widgets/dropzone/dropzone.dart';
 import '../../widgets/text_box/text_box.dart';
 
