387067acd4893012cf5c3b4dfbcf6e5e2b112e07
dominego <doiran98@gmail.com>
2022-01-19 11:23:23 +0100

    Hover

--- lib/widgets/call_to_action/call_to_action_tablet_desktop.dart
+++ lib/widgets/call_to_action/call_to_action_tablet_desktop.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:non_fungible_royalty_token_marketplace_ui/locator.dart';
 import 'package:non_fungible_royalty_token_marketplace_ui/services/navigation_service.dart';
+import 'package:non_fungible_royalty_token_marketplace_ui/widgets/hover/on_hover_button.dart';
 import '../../../../constants/app_colors.dart';
 
 class CallToActionTabletDesktop extends StatelessWidget {
@@ -10,26 +11,28 @@ class CallToActionTabletDesktop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => {
-        locator<NavigationService>().navigateTo("wallet"),
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 10),
-        child: Text(
-          title,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w800,
-            color: Colors.white,
+    return OnHoverButton(
+      child: ElevatedButton(
+        onPressed: () => {
+          locator<NavigationService>().navigateTo("wallet"),
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-      style: ElevatedButton.styleFrom(
-        primary: primaryColor,
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(color: primaryColor),
-          borderRadius: BorderRadius.circular(10),
+        style: ElevatedButton.styleFrom(
+          primary: primaryColor,
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(color: primaryColor),
+            borderRadius: BorderRadius.circular(10),
+          ),
         ),
       ),
     );
