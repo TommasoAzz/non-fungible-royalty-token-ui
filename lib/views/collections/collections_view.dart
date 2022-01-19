f6fc3759f5ca8e31969384b34b11ff30eb568e27
dominego <doiran98@gmail.com>
2022-01-19 09:53:04 +0100

    Add tablet view in collection

--- lib/views/collections/collections_view.dart
+++ lib/views/collections/collections_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:non_fungible_royalty_token_marketplace_ui/views/collections/collection_content_tablet.dart';
 import 'collections_content_mobile.dart';
-import 'collections_content_tablet_desktop.dart';
+import 'collections_content_desktop.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class CollectionsView extends StatelessWidget {
